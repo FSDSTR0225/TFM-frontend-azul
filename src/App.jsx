912846c37fdeb9567e945f046c0a79ed49e61c61
toomajbandad <tbandad@gmail.com>
2025-04-14 21:19:48 +0200

    finalize register & login page and register.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./styles/reset.css";
 import "./App.css";
-import Register from "./pages/Register";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import { Routes, Route } from "react-router";
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
