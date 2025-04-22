5d6f60c229eb32c26d86f4ab269980a8e053ef65
Salva <sgorlatt@gmail.com>
2025-04-22 17:36:14 +0200

    Home 2.0

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../style/Home.css";
 import EventList from "../components/EventList";
 import UserList from "../components/UserList";
 import "../style/EventList.css";
 import "../style/UserList.css";
+import BotSection from "../components/BotSection";
+import IntroSection from "../components/IntroSection";
 
 export default function Home() {
+  const contentRef = useRef();
+
   return (
     <div className="home-container">
       <div className="welcome">
         <h1>Bienvenido a Link2Play</h1>
       </div>
-      <div className="description">
-        <p>
-          "Encuentra jugadores,crea partidas,sube de nivel y haz que grindear no
-          sea aburrido"
-        </p>
-      </div>
-      <>
+      <IntroSection scrollRef={contentRef} />
+      <div ref={contentRef}>
         <EventList />
         <UserList />
-      </>
+        <BotSection />
+      </div>
     </div>
   );
 }
