8c409c94ac1df79fe075faef7b712e6000dcbf9c
Salva <sgorlatt@gmail.com>
2025-04-26 21:07:39 +0200

    add fix

--- src/pages/Lobby.jsx
+++ src/pages/Lobby.jsx
@@ -1,26 +1,7 @@
-import React, { useRef } from "react";
-import "../style/Lobby.css";
-import EventList from "../components/EventList";
-import UserList from "../components/UserList";
-import "../style/EventList.css";
-import "../style/UserList.css";
-import BotSection from "../components/BotSection";
-import IntroSection from "../components/IntroSection";
+import React from "react";
 
-export default function Home2() {
-  const contentRef = useRef();
-
-  return (
-    <div className="home-container">
-      <div className="welcome">
-        <h1>Bienvenido a Link2Play</h1>
-      </div>
-      <IntroSection scrollRef={contentRef} />
-      <div ref={contentRef} className="sub-container">
-        <EventList />
-        <UserList />
-        <BotSection />
-      </div>
-    </div>
-  );
+function Lobby() {
+  return <div>Lobby</div>;
 }
+
+export default Lobby;
