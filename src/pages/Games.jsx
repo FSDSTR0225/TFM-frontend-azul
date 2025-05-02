ec8e84fafc1cd1fea32a75eb0451ce2dcaf28b07
Salva <sgorlatt@gmail.com>
2025-05-02 12:33:11 +0200

    commit antes del merge

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PlatformList from "../components/PlatformList";
 import "../style/Games.css";
-
+import AllGames from "../components/AllGames";
 function Games() {
   return (
-    <div className="games-container">
-      <PlatformList />
+    <div className="games-page">
+      <section className="platforms-section">
+        <PlatformList />
+      </section>
+      <section className="games-section">
+        <AllGames />
+      </section>
     </div>
   );
 }
