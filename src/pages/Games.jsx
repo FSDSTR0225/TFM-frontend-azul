b71fddada33e3f611d0a046214964ced02e3ee29
Salva <sgorlatt@gmail.com>
2025-05-03 10:54:23 +0200

    prueba1

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -3,16 +3,15 @@ import { Link } from "react-router-dom";
 import PlatformList from "../components/PlatformList";
 import "../style/Games.css";
 import AllGames from "../components/AllGames";
+
 function Games() {
   return (
-    <div className="games-page">
-      <section className="platforms-section">
-        <PlatformList />
-      </section>
-      <section className="games-section">
+    <section className="games-page">
+      <PlatformList />
+      <div className="games-section">
         <AllGames />
-      </section>
-    </div>
+      </div>
+    </section>
   );
 }
 
