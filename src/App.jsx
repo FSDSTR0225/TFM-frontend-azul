388c31907da67ed740f7e9bc31bef560dc453e72
toomajbandad <tbandad@gmail.com>
2025-05-06 19:38:36 +0200

    create new navbar and get me with token

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles/reset.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import GamesByPlatform from "./pages/GamesByPlatform";
@@ -8,7 +8,7 @@ import Games from "./pages/Games";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import NavBar from "./components/NavBar";
+// import NavBar from "./components/NavBar";
 import SearchInputExplore from "./components/searchInputExplore";
 import Events from "./pages/Events";
 import Profile from "./pages/Profile";
@@ -16,6 +16,7 @@ import GameDetails from "./pages/GameDetails";
 import Lobby from "./pages/Lobby";
 
 import AuthContext from "./context/AuthenticationContext";
+import TopNavbar from "./components/TopNavbar/TopNavbar";
 
 export default function App() {
   const [showSearch, setShowSearch] = useState(false);
@@ -24,6 +25,29 @@ export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false); //state to show user is logged in or now
   const [token, setToken] = useState(false); //state to show user token
   const [userInfos, setUserInfos] = useState({}); //state to show current user data
+
+  useEffect(() => {
+    let userLocal = localStorage.getItem("user");
+    if (userLocal) {
+      let userToken = JSON.parse(userLocal).token;
+      fetch("http://localhost:3000/users/me", {
+        method: "GET",
+        headers: { authorization: `Bearer ${userToken}` },
+      })
+        .then((res) => {
+          if (res.ok === true) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          console.log(data);
+          login(data.user, userToken);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, []);
 
   const login = (userInfos, token) => {
     //process of loging in a user
@@ -54,7 +78,12 @@ export default function App() {
       }}
     >
       <div className="App">
-        <NavBar showSearch={showSearch} setShowSearch={setShowSearch} />
+        {/* <NavBar showSearch={showSearch} setShowSearch={setShowSearch} /> */}
+        <TopNavbar
+          showSearch={showSearch}
+          setShowSearch={setShowSearch}
+          setSearch={setSearch}
+        />
         {showSearch && (
           <SearchInputExplore
             search={search}
---
+++ src/components/TopNavbar/TopNavbar.jsx
@@ -0,0 +1,126 @@
+import React, { useEffect, useState, useContext } from "react";
+import "./TopNavbar.css";
+import { NavLink, useNavigate } from "react-router-dom";
+import AuthContext from "../../context/AuthenticationContext";
+import { FaSearch } from "react-icons/fa";
+import blankImg from "/images/profile/blankImg.jpg";
+
+export default function TopNavbar({ showSearch, setShowSearch, setSearch }) {
+  const authContext = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const [platforms, setPlatforms] = useState([]);
+
+  const Url = "http://localhost:3000/platforms";
+
+  function fetchPlatforms() {
+    fetch(Url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Network response was not ok (${response.status})`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPlatforms(data.platforms);
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  }
+
+  useEffect(() => {
+    fetchPlatforms();
+  }, []);
+
+  const toggleExplore = () => {
+    setShowSearch(!showSearch);
+    if (showSearch) {
+      setSearch("");
+    }
+  };
+
+  return (
+    <div className="TopNavbar">
+      <div className="TopNavbar__container">
+        <div className="TopNavbar__logoWrapper">
+          <NavLink to="/" className="Topnavbar__logoItem">
+            Link2Play
+          </NavLink>
+        </div>
+        <div className="Topnavbar__menuItems">
+          <NavLink to="/games" className="Topnavbar__menuItem">
+            Juegos
+            <div className="Topnavbar__dropDown">
+              {platforms.map((platform) => (
+                <NavLink
+                  className="Topnavbar__dropDown__Item"
+                  key={platform._id}
+                  to={`/platforms/${platform._id}/games`}
+                >
+                  {platform.name}
+                </NavLink>
+              ))}
+            </div>
+          </NavLink>
+
+          <NavLink to="/events" className="Topnavbar__menuItem">
+            Eventos
+          </NavLink>
+          <NavLink to="/post" className="Topnavbar__menuItem">
+            Comunidad
+          </NavLink>
+          <NavLink
+            to="/"
+            className="Topnavbar__menuItem"
+            onClick={toggleExplore}
+          >
+            Search
+            <FaSearch className="searchIcon" />
+          </NavLink>
+        </div>
+        <div className="Topnavbar__loginItems">
+          {!authContext.isLoggedIn ? (
+            <NavLink to="/login" className="Topnavbar__loginItem">
+              Login / Signup
+            </NavLink>
+          ) : (
+            <>
+              <div className="Topnavbar__profileItem">
+                {authContext.userInfos.username}
+                <img className="profileImg" src={blankImg} />
+                <div className="profile__dropDown">
+                  <NavLink
+                    to="/profile"
+                    className="Topnavbar__profileDropDown__item"
+                  >
+                    Editar perfil
+                  </NavLink>
+                  <NavLink
+                    to="/solicitudes"
+                    className="Topnavbar__profileDropDown__item"
+                  >
+                    Solicitudes de juego
+                  </NavLink>
+                  <NavLink
+                    to="/configuracion"
+                    className="Topnavbar__profileDropDown__item"
+                  >
+                    Configuración
+                  </NavLink>
+                  <NavLink
+                    to="/"
+                    className="Topnavbar__profileDropDown__item"
+                    onClick={() => authContext.logout()}
+                  >
+                    Cerrar sesión
+                  </NavLink>
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
