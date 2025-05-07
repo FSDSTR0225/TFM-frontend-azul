15d010ac44acd758090a01a2900a01d4440b29ad
Salva <sgorlatt@gmail.com>
2025-05-07 13:01:33 +0200

    privateRoute

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,14 +1,26 @@
+import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import AuthContext from "../context/AuthContext";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useAuth(); //Asi obtenemos al usuario(datos recogidos del contexto)
+  const { isLoggedIn } = useContext(AuthContext); //Asi obtenemos al usuario(datos recogidos del contexto)
 
-  if (!user) {
-    return <Navigate to="/login" replace />; // replace evita que pueda volver atras el usuario
-  }
-
-  return children;
+  return isLoggedIn ? children : <Navigate to="/login" replace />; // replace evita que pueda volver atras el usuario
 };
 
 export default PrivateRoute;
+
+// import { Navigate } from "react-router-dom";
+// import { useAuth } from "../context/AuthContext";
+
+// const PrivateRoute = ({ children }) => {
+//   const { user } = useAuth(); //Asi obtenemos al usuario(datos recogidos del contexto)
+
+//   if (!user) {
+//     return <Navigate to="/login" replace />; // replace evita que pueda volver atras el usuario
+//   }
+
+//   return children;
+// };
+
+// export default PrivateRoute;
